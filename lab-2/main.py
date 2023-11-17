eae690ae470bbaee84f6d9211251da2783910b7e
sixaliyevas <anar.sixaliyev@kapitalbank.az>
2023-11-16 23:17:53 -0500

    added lab 2

---
+++ lab-2/main.py
@@ -0,0 +1,335 @@
+##Structure is protected from LAB 1, such as we will be focusing on the reshape and render functions
+## Program will read the inputs ( Geometric data for an object, Set of control points, the type (Catmull-Rom or B), and the spacing (dt))
+## from the input_file.txt file under the inputs folder. File has to be in the below format:
+## First line must contain the value of the dt
+## Second line must contain the value of the Algorithm to be used - B-spline(BS) or Catmull-Rom(CMS)
+## Starting from the third line and forward will contain the geometric data of the object.
+## No need to spesify if the coordinates given with  Fixed Angles or Quaternions, code can identify it(by checking length) and process it by itself.
+import numpy as np
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+
+# Global variables
+g_screenWidth = 0
+g_screenHeight = 0
+g_frameIndex = 0
+g_angle = 0
+current_frame = 0
+num_frames = 100
+
+
+# Initialize function
+def init():
+    pass
+
+# The same, repeated function is extracted form the render to simplify it
+def prepareRender():
+    # Clear the OpenGL buffer
+    glClearColor(0.0, 0.0, 0.0, 0.0)
+    glClearDepth(1.0)
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+    # Configure rendering settings
+    glEnable(GL_DEPTH_TEST)
+    glShadeModel(GL_SMOOTH)
+
+    # Enable lighting and set light source attributes
+    glEnable(GL_LIGHTING)
+    glEnable(GL_LIGHT0)
+
+    # Light source attributes
+    LightAmbient = [0.4, 0.4, 0.4, 1.0]
+    LightDiffuse = [0.3, 0.3, 0.3, 1.0]
+    LightSpecular = [0.4, 0.4, 0.4, 1.0]
+    LightPosition = [5.0, 5.0, 5.0, 1.0]
+
+    # Configure OpenGL light source
+    glLightfv(GL_LIGHT0, GL_AMBIENT, LightAmbient)
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, LightDiffuse)
+    glLightfv(GL_LIGHT0, GL_SPECULAR, LightSpecular)
+    glLightfv(GL_LIGHT0, GL_POSITION, LightPosition)
+
+    # Surface material attributes
+    material_Ka = [0.11, 0.06, 0.11, 1.0]
+    material_Kd = [0.43, 0.47, 0.54, 1.0]
+    material_Ks = [0.33, 0.33, 0.52, 1.0]
+    material_Ke = [0.1, 0.0, 0.1, 1.0]
+    material_Se = 10
+
+    # Set material properties
+    glMaterialfv(GL_FRONT, GL_AMBIENT, material_Ka)
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, material_Kd)
+    glMaterialfv(GL_FRONT, GL_SPECULAR, material_Ks)
+    glMaterialfv(GL_FRONT, GL_EMISSION, material_Ke)
+    glMaterialf(GL_FRONT, GL_SHININESS, material_Se)
+
+
+# New global variable
+g_leg_angle = 0
+
+# New function for leg motion
+def generate_leg_motion(t):
+    global g_leg_angle
+    #sinusoidal motion for the leg
+    leg_speed_scale = dt * 100.0
+    leg_motion = np.sin(2 * np.pi * t * leg_speed_scale)
+    g_leg_angle = leg_motion * 45 # Adjust the amplitude and scale as needed
+    print('===> leg angle ', g_leg_angle)
+# Modify render_left_leg and render_right_leg functions
+def render_left_leg(position):
+    glPushMatrix()
+    glTranslatef(position[0] - 0.3, position[1] - 1, position[2])  # Adjust the position
+    glRotatef(g_leg_angle, 1, 0, 0)  # Rotate the leg based on the generated motion
+
+    glScalef(0.5, 4, 0.5)
+
+    glutSolidCube(1)
+
+    glPopMatrix()
+
+def render_right_leg(position):
+    glPushMatrix()
+    glTranslatef(position[0] + 0.3, position[1] - 1, position[2])  # Adjust the position
+    glRotatef(-g_leg_angle, 1, 0, 0)  # Inverse rotation for the other leg
+    glScalef(0.5, 4, 0.5)
+
+    glutSolidCube(1)
+    glPopMatrix()
+
+# Modify the render function
+def render():
+    global current_frame
+    prepareRender()
+
+    # Interpolate position and orientation based on 'algo' variable
+    if algo == "CMS\n":
+        position, orientation = interpolate_catmull_rom(current_frame / (num_frames - 1))
+    elif algo == 'BS\n':
+        position, orientation = interpolate_B_spline(current_frame / (num_frames - 1))
+    else:
+        print("Invalid algo format in the input file:", algo)
+        sys.exit()
+
+    # Generate leg motion
+    generate_leg_motion(current_frame / (num_frames - 1))
+
+    # Render the figure including torso and legs
+    render_figure(position)
+
+    # Increment current frame
+    current_frame += dt
+    if current_frame >= num_frames:
+        current_frame = 0
+
+    # Disable lighting
+    glDisable(GL_LIGHT0)
+    glDisable(GL_LIGHTING)
+
+    # Swap back and front buffers
+    glutSwapBuffers()
+
+# Modify render_figure function to include rendering of legs
+def render_figure(position):
+    glPushMatrix()
+    glTranslatef(position[0], position[1], position[2])
+
+    # Render the torso
+    render_torso()
+    # Render the legs
+    render_left_leg(position)
+    render_right_leg(position)
+
+    glPopMatrix()
+
+# ... (existing code)
+
+def render_torso():
+    glPushMatrix()
+
+    # Set the color and material properties for the torso
+    glColor3f(0.5, 0.5, 1.0)
+    material_Ka_torso = [0.1, 0.1, 0.1, 1.0]
+    material_Kd_torso = [0.4, 0.4, 0.4, 1.0]
+    material_Ks_torso = [0.5, 0.5, 0.5, 1.0]
+    material_Ke_torso = [0.1, 0.0, 0.1, 1.0]
+    material_Se_torso = 10
+
+    # Set material properties for the torso
+    glMaterialfv(GL_FRONT, GL_AMBIENT, material_Ka_torso)
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, material_Kd_torso)
+    glMaterialfv(GL_FRONT, GL_SPECULAR, material_Ks_torso)
+    glMaterialfv(GL_FRONT, GL_EMISSION, material_Ke_torso)
+    glMaterialf(GL_FRONT, GL_SHININESS, material_Se_torso)
+    # Render the torso as a cube
+    glutSolidCube(1)
+    glPopMatrix()
+
+def quaternion_to_matrix(quaternion):
+    w, x, y, z = quaternion
+    return [
+        [1 - 2 * y * y - 2 * z * z, 2 * x * y - 2 * z * w, 2 * x * z + 2 * y * w, 0],
+        [2 * x * y + 2 * z * w, 1 - 2 * x * x - 2 * z * z, 2 * y * z - 2 * x * w, 0],
+        [2 * x * z - 2 * y * w, 2 * y * z + 2 * x * w, 1 - 2 * x * x - 2 * y * y, 0],
+        [0, 0, 0, 1]
+    ]
+
+
+# Catmull-Rom spline interpolation
+def interpolate_catmull_rom(t):
+    # Interpolate position and orientation based on control points
+    n = len(control_points)
+    i = int(t * (n - 1))
+    t = t * (n - 1) - i
+
+    p0 = np.array(control_points[(i - 1) % n][:3])
+    p1 = np.array(control_points[i][:3])
+    p2 = np.array(control_points[(i + 1) % n][:3])
+    p3 = np.array(control_points[(i + 2) % n][:3])
+
+    position = catmull_rom(t, p0, p1, p2, p3)
+
+    o0 = np.array(control_points[(i - 1) % n][3:])
+    o1 = np.array(control_points[i][3:])
+    o2 = np.array(control_points[(i + 1) % n][3:])
+    o3 = np.array(control_points[(i + 2) % n][3:])
+
+    orientation = catmull_rom(t, o0, o1, o2, o3)
+
+    return position, orientation
+
+
+# Calculate Catmull-Rom interpolation
+def catmull_rom(t, p0, p1, p2, p3):
+    return 0.5 * ((2 * p1) + (-p0 + p2) * t + (2 * p0 - 5 * p1 + 4 * p2 - p3) * t ** 2 + (
+            -p0 + 3 * p1 - 3 * p2 + p3) * t ** 3)
+
+
+# B-spline interpolation
+def interpolate_B_spline(t):
+    # Interpolate position and orientation based on control points
+    n = len(control_points)
+    i = int(t * (n - 1))
+    t = t * (n - 1) - i
+
+    p0 = np.array(control_points[(i - 1) % n][:3])
+    p1 = np.array(control_points[i][:3])
+    p2 = np.array(control_points[(i + 1) % n][:3])
+    p3 = np.array(control_points[(i + 2) % n][:3])
+
+    position = b_spline(t, p0, p1, p2, p3)
+
+    o0 = np.array(control_points[(i - 1) % n][3:])
+    o1 = np.array(control_points[i][3:])
+    o2 = np.array(control_points[(i + 1) % n][3:])
+    o3 = np.array(control_points[(i + 2) % n][3:])
+
+    orientation = b_spline(t, o0, o1, o2, o3)
+
+    return position, orientation
+
+
+# Calculate B-spline interpolation
+def b_spline(t, p0, p1, p2, p3):
+    # The B-spline matrix
+    matrix = np.array([
+        [-1.0 / 6, 3.0 / 6, -3.0 / 6, 1.0 / 6],
+        [3.0 / 6, -6.0 / 6, 3.0 / 6, 0.0],
+        [-3.0 / 6, 0.0, 3.0 / 6, 0.0],
+        [1.0 / 6, 4.0 / 6, 1.0 / 6, 0.0]
+    ])
+
+    t_vector = np.array([t ** 3, t ** 2, t, 1])
+    control_points_matrix = np.array([p0, p1, p2, p3])
+
+    result = np.dot(t_vector, np.dot(matrix, control_points_matrix))
+
+    return result
+
+
+# Keyboard input function
+def keyboard(key, x, y):
+    if key == b'\x1b' or key == b'q':
+        sys.exit()
+
+
+# Reshape function
+def reshape(w, h):
+    global g_screenWidth, g_screenHeight
+    g_screenWidth = w
+    g_screenHeight = h
+
+    # Viewport
+    glViewport(0, 0, w, h)
+
+    # Projection matrix
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(45.0, float(w) / float(h), 0.01, 5000.0)
+
+
+# Timer function
+def timer(value):
+    glutPostRedisplay()
+    glutTimerFunc(60, timer, 0)  # 16 ms per frame (about 60 frames per second)
+
+
+# Load data from an input file
+def load_data_from_file(file_path):
+    global control_points, dt, algo
+
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    dt = float(lines[0])
+    algo = lines[1]
+    control_points = []
+    for i in range(2, len(lines)):
+        data = lines[i].split()
+        if lines[i].startswith("#"):
+            continue  # Skip comments
+
+        if len(data) == 6:
+            x, y, z, rx, ry, rz = map(float, data)
+            control_points.append((x, y, z, rx, ry, rz))
+        elif len(data) == 7:
+            x, y, z, rx, ry, rz, rw = map(float, data)
+            control_points.append((x, y, z, rx, ry, rz, rw))
+        else:
+            print("Invalid data format in the input file.")
+            sys.exit()
+
+    if len(control_points) < 4:
+        print("You need at least 4 control points for interpolation.")
+        sys.exit()
+
+
+# Main function
+def main():
+    global g_screenWidth, g_screenHeight
+
+    # Load data from an input file
+    load_data_from_file('inputs/input_file')
+
+    # Create an OpenGL window
+    glutInit(sys.argv)
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
+    glutInitWindowSize(600, 600)
+    glutInitWindowPosition(100, 100)
+    glutCreateWindow(sys.argv[0])
+
+    # Initialize
+    init()
+
+    # Set callback functions
+    glutDisplayFunc(render)
+    glutReshapeFunc(reshape)
+    glutKeyboardFunc(keyboard)
+    glutTimerFunc(60, timer, 0)
+
+    # Main loop
+    glutMainLoop()
+
+
+if __name__ == "__main__":
+    main()
